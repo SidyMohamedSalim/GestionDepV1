0956b6ac4aa1458f70a77229176f432fa297987b
Salim <sidymamadousalim@gmail.com>
2024-02-08 12:06:46 +0100

    fix bug

--- resources/views/admin/enseignant/index.blade.php
+++ resources/views/admin/enseignant/index.blade.php
@@ -52,24 +52,46 @@
                         @endforelse
 
                     </td>
-                    <td class="px-6 py-4">
+                    <td class="flex items-center px-6 py-4 justify-evenly">
                         <a href="{{ route('enseignant.edit', $enseignant) }}"
-                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="lucide lucide-notebook-pen">
+                                <path d="M13.4 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-7.4" />
+                                <path d="M2 6h4" />
+                                <path d="M2 10h4" />
+                                <path d="M2 14h4" />
+                                <path d="M2 18h4" />
+                                <path d="M18.4 2.6a2.17 2.17 0 0 1 3 3L16 11l-4 1 1-4Z" />
+                            </svg>
+                        </a>
 
                         <x-enseignant.affectation-bureau :enseignant="$enseignant" />
 
+                        {{-- delete --}}
                         <div class="space-y-6 text-red-500 ">
                             <button x-data=""
-                                x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion{{ $enseignant->id }}')">{{ __('Delete') }}</button>
+                                x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion{{ $enseignant->id }}')">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="lucide lucide-trash-2">
+                                    <path d="M3 6h18" />
+                                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                    <line x1="10" x2="10" y1="11" y2="17" />
+                                    <line x1="14" x2="14" y1="11" y2="17" />
+                                </svg>
+                            </button>
 
                             <x-modal name="confirm-user-deletion{{ $enseignant->id }}" focusable>
-                                <form method="post" action="{{ route('enseignant.destroy', $enseignant) }}"
-                                    class="p-6">
+                                <form method="post" action="{{ route('enseignant.destroy', $enseignant) }}" class="p-6">
                                     @csrf
                                     @method('delete')
 
                                     <h2 class="text-lg font-medium text-gray-900">
-                                        {{ __('Are you sure you want to delete ?') }}
+                                        {{ __('Voulez-vous Vraiment  Supprimer ?') }}
                                     </h2>
 
                                     <div class="flex justify-end mt-6">
